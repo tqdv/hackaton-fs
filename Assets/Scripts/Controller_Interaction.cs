6b30b1dc1a73b5e6ed0c7af84c74f7cba059b61b
Groupe <tilwa.qendov@gmail.com>
2020-01-30 18:40:41 +0100

    Ca marche

--- Assets/Scripts/Controller_Interaction.cs
+++ Assets/Scripts/Controller_Interaction.cs
@@ -15,28 +15,48 @@ public class Controller_Interaction : MonoBehaviour
 
     void Start()
     {
-        device = WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right);
+        //device = WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right);
+        device = WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_HMD);
         pivot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         pivot.transform.localScale = 0.005f * Vector3.one;
         pivot.name = "Pivot";
         pivot.SetActive(false);
+        // WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right).GetAxis(WaveVR_ButtonList.EButtons.Touchpad);
     }
+
+    
+
 
     // Update is called once per frame
     void Update()
     {
-        if (device.GetPressDown(WVR_InputId.WVR_InputId_Alias1_Trigger))
+        //GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = (device.transform.pos).ToString();
+
+        if (device.GetPress(WVR_InputId.WVR_InputId_Alias1_Trigger))
         {
+            
             Vector3 direction = device.transform.rot * Vector3.forward;
             RaycastHit hit;
-            LayerMask layer = 0 << 8; 
-            if (Physics.Raycast(device.transform.pos, direction, out hit,Mathf.Infinity,layer))
-            {
-                //GameObject.Find("Debug").GetComponent<TextMesh>().text = "Yes";
-                // Grab(hit);
-                Destroy(hit.collider.gameObject);
-            }
+            LayerMask layer = 0 << 8;
 
+            Debug.DrawRay(device.transform.pos, 10.0f * direction);
+            
+
+            //if (Physics.Raycast(device.transform.pos, direction, out hit,Mathf.Infinity,layer))
+            if (Physics.Raycast(device.transform.pos, 10.0f * direction))
+            {
+                //GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = "touche";
+                // Grab(hit);
+                //Destroy(hit.collider.gameObject);
+            }
+            else
+            {
+                GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = "";
+            }
+        }
+        else
+        {
+            GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = "";
         }
 
         if (device.GetPressUp(WVR_InputId.WVR_InputId_Alias1_Trigger) && selected != null)
---
+++ Assets/toto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, 1000.0f))
+        {
+            GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = "touche";
+        }
+        else
+        {
+            GameObject.Find("Debug").GetComponent<TMPro.TextMeshPro>().text = "rate";
+        }
+    }
+}

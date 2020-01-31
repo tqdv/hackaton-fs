5bcb027a48ffba2171488fe053d359462d66f3ce
Tilwa Qendov <tilwa.qendov@gmail.com>
2020-01-31 11:48:53 +0100

    Add Destruction Ray

---
+++ Assets/Scripts/DestructionRay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Sends a ray from the given object that destroys an object on click
+public class DestructionRay : MonoBehaviour
+{
+
+	public int layerNum = Physics.DefaultRaycastLayers;
+	int layerMask;
+	RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		layerMask = 1 << layerNum;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 dir = transform.TransformDirection(Vector3.forward);
+
+		// Raycast
+		bool touched = Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask);
+
+		// Display Ray
+		Debug.DrawRay(transform.position, dir * 5 /* distance */);
+		
+		// Highlight target ? TODO
+
+		// Destroy on click
+		if (touched && true /* FIXME click */) {
+			Object.Destroy(hit.transform); /* .parent maybe ? */
+
+			// Explosions
+		}
+        
+    }
+}
---
+++ Assets/Scripts/PlayDestroyAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayDestroyAudio : MonoBehaviour
+{
+	AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	sound = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnDestroy ()
+	{
+		sound.Play();
+	}
+}

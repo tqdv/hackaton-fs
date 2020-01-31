1517e1f00e224e5dece96a178213f77a6c77eea6
Groupe <tilwa.qendov@gmail.com>
2020-01-31 12:53:24 +0100

    Destroy pages it works !

--- Assets/Scripts/DestructionRay.cs
+++ Assets/Scripts/DestructionRay.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 // Sends a ray from the given object that destroys an object on click
 public class DestructionRay : MonoBehaviour
@@ -13,8 +14,7 @@ public class DestructionRay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		layerMask = 1 << layerNum;
-        
+		layerMask = 1 << layerNum;  
     }
 
     // Update is called once per frame
@@ -26,13 +26,18 @@ public class DestructionRay : MonoBehaviour
 		bool touched = Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask);
 
 		// Display Ray
-		Debug.DrawRay(transform.position, dir * 5 /* distance */);
-		
-		// Highlight target ? TODO
+		Debug.DrawRay(transform.position, dir * 5 /* distance */, Color.white, 5 /* secs */);
+        // Highlight target ? TODO
 
-		// Destroy on click
-		if (touched && true /* FIXME click */) {
-			Object.Destroy(hit.transform); /* .parent maybe ? */
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = transform.position + dir * 0.1f;
+        go.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+
+        // Destroy on click
+        if (touched && Input.GetButtonDown("TouchPadLeft")) {
+            print("touche");
+            Destroy(hit.transform.parent.gameObject);
 
 			// Explosions
 		}
